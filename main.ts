ba8dba6a026206a60c73bd1517b6dc854b39f5c0
9500levis <84763193+9500levis@users.noreply.github.com>
2021-06-07 17:37:22 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
+game.startCountdown(40000)
+let Coin = game.createSprite(randint(0, 4), randint(0, 4))
+Coin.set(LedSpriteProperty.Brightness, 180)
 let CHAR = game.createSprite(2, 2)
+while (true) {
+    if (true) {
+        game.addScore(1)
+        Coin.set(LedSpriteProperty.X, randint(0, 4))
+        Coin.set(LedSpriteProperty.Y, randint(0, 4))
+    }
+    if (input.acceleration(Dimension.X) > 0) {
+        CHAR.change(LedSpriteProperty.X, 1)
+        basic.pause(100)
+    }
+    if (input.acceleration(Dimension.X) < 0) {
+        CHAR.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+    }
+    if (input.acceleration(Dimension.Y) > 0) {
+        CHAR.change(LedSpriteProperty.Y, 1)
+        basic.pause(100)
+    }
+    if (input.acceleration(Dimension.X) < 0) {
+        CHAR.change(LedSpriteProperty.X, -1)
+        basic.pause(100)
+    }
+}
